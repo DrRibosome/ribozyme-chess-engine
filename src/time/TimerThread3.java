904c093d796a05bd146fa8fe3e71ed10773558b9
Jack Crawford <jdc2172@gmail.com>
2013-02-21 16:36:27 -0500

    tightened time controls some

--- src/time/TimerThread3.java
+++ src/time/TimerThread3.java
@@ -59,7 +59,7 @@ public final class TimerThread3 extends Thread{
 		final int material = getMaterial(s);
 		long target = time / (getHalfMovesRemaining(material)/2);
 		search.setListener(l);
-		final long maxTime = (long)(time*(material >= 60? .03: .06));
+		final long maxTime = (long)(time*(material >= 60? .04: .06));
 		
 		System.out.println("moves remaining = "+(getHalfMovesRemaining(material)/2));
 		System.out.println("target time = "+target);
