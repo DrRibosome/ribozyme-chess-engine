2c69c1f042d51312a844f34a7280bf2220fd225d
Jack Crawford <jdc2172@gmail.com>
2013-02-21 22:27:16 -0500

    deleted old, bad experimental search

--- src/search/exp/searchV32c/Encoder.java
+++
@@ -1,9 +0,0 @@
-package search.exp.searchV32c;
-
-public interface Encoder <T>{
-	/** encode the data in long[] store, first index MUST be the key*/
-	public void encode(T t, long[] store);
-	public void load(T t, long[] store);
-	/** get the target size of the store*/
-	public int requiredStoreSize();
-}
