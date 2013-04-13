6c93a1b1d1ff0c1505d85528197421782159ca59
Jack Crawford <jdc2172@gmail.com>
2013-04-13 04:49:31 -0400

    changed genetic base clsas

--- src/util/analysis/GauntletP.java
+++ src/util/analysis/GauntletP.java
@@ -12,7 +12,7 @@ import state4.StateUtil;
 import util.opening2.Book;
 import eval.Evaluator2;
 import eval.expEvalV3.E4;
-import eval.expEvalV3.E4v2;
+import eval.expEvalV3.ExpEvalV3v4;
 import eval.expEvalV3.gparams.GParams1v2;
 
 /**
@@ -135,10 +135,10 @@ public class GauntletP {
 					//new ExpEvalV3v4(GParams3.buildEval());
 					//new SuperEvalS4V10();
 					//new E4(GParams3Base.buildEval());
-					//new ExpEvalV3v4(GParams3.buildEval());
+					new ExpEvalV3v4(GParams1v2.buildEval());
 					//new SuperEvalS4V8();
 					//new ExpEvalV1();
-					new E4v2(GParams1v2.buildEval());
+					//new E4v2(GParams1v2.buildEval());
 			
 			final Search4[] search = new Search4[2];
 			search[0] = new SearchS4V33t(e1, hashSize, false);
