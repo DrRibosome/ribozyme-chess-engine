db48efccf23f6529ccb4ff8b6455f2690f3ab6ba
Jack Crawford <jdc2172@gmail.com>
2013-06-02 19:29:48 -0400

    added basis classed for approx eval implementation

---
+++ src/eval/ScoreEncoder.java
@@ -0,0 +1,57 @@
+package eval;
+
+/** encoder for score, margin, and any flags*/
+public final class ScoreEncoder {
+	private final static int scoreBits = 16;
+	private final static int marginBits = 10;
+	private final static int flagBits = 6;
+	
+	private final static int scoreMask;
+	private final static int marginMask;
+	private final static int flagMask;
+	private final static int scoreMaskOffset;
+	private final static int marginMaskOffset;
+	private final static int flagMaskOffset;
+	
+	static{
+		scoreMaskOffset = 0;
+		marginMaskOffset = scoreBits;
+		flagMaskOffset = scoreBits + marginBits;
+		
+		{
+			int temp = 0;
+			for(int a = 0; a < scoreBits; a++) temp |= 1 << (scoreMaskOffset + a);
+			scoreMask = temp;
+		}
+		{
+			int temp = 0;
+			for(int a = 0; a < scoreBits; a++) temp |= 1 << (marginMaskOffset + a);
+			marginMask = temp;
+		}
+		{
+			int temp = 0;
+			for(int a = 0; a < scoreBits; a++) temp |= 1 << (flagMaskOffset + a);
+			flagMask = temp;
+		}
+	}
+	
+	public static int getScore(final int scoreEncoding){
+		return scoreEncoding & scoreMask;
+	}
+	
+	public static int getMargin(final int scoreEncoding){
+		return (scoreEncoding & marginMask) >>> marginMaskOffset;
+	}
+	
+	public static int getFlags(final int scoreEncoding){
+		return (scoreEncoding & flagMask) >>> flagMaskOffset;
+	}
+	
+	public static int encode(final int score, final int margin, final int flags){
+		assert score < 1<<scoreBits;
+		assert margin < 1<<marginBits;
+		assert flags < 1<<flagBits;
+		
+		return score | (margin << marginMaskOffset) | (flags << flagMaskOffset);
+	}
+}
