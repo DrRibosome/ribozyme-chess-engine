dc7c841a51fea12526165c4381d6cf893f094bd3
Jack Crawford <jdc2172@gmail.com>
2013-08-12 12:10:54 -0400

    added better comments

--- src/search/search34/Search34v4.java
+++ src/search/search34/Search34v4.java
@@ -55,7 +55,7 @@ public final class Search34v4 implements Search4{
 		public boolean skipNullMove = false;
 		/** holds killer moves as first 12 bits (ie, masked 0xFFF) of move encoding*/
 		public final long[] killer = new long[2];
-		/** move played one ply back, 0 if no move played (eg, for root node, null move, etc)*/
+		/** controls whether we should futility prune*/
 		public boolean futilityPrune;
 		
 		{
