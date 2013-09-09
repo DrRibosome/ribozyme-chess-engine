0162db4982f054d4f0d773fc12d602ca9d002f52
Jack Crawford <jdc2172@gmail.com>
2013-09-08 20:34:11 -0400

    mobility eval separated

--- src/eval/e9/E9v3.java
+++ src/eval/e9/E9v3.java
@@ -7,6 +7,7 @@ import state4.State4;
 import eval.Evaluator3;
 import eval.PositionMasks;
 import eval.ScoreEncoder;
+import eval.e9.mobilityEval.MobilityEval;
 import eval.e9.pawnEval.PawnEval;
 
 public final class E9v3 implements Evaluator3{
@@ -17,6 +18,17 @@ public final class E9v3 implements Evaluator3{
 	private final static int stage3Flag = 1 << 2;
 	private final static int evalCompleteMask = stage1Flag | stage2Flag | stage3Flag;
 	
+	private final static int[] kingDangerTable;
+	
+	private final static int[] materialWeights = new int[7];
+	
+	private final static int tempoWeight = S(14, 5);
+	private final static int bishopPairWeight = S(10, 42);
+
+	private final static int[] zeroi = new int[2];
+	
+	private final int[] materialScore = new int[2];
+
 	/**
 	 * gives bonus multiplier to the value of sliding pieces
 	 * mobilitiy scores based on how cluttered the board is
@@ -27,19 +39,6 @@ public final class E9v3 implements Evaluator3{
 	 * indexed [num-pawn-attacked-squares]
 	 */
 	private final static double[] clutterIndex; 
-	
-	private final static int[] kingDangerTable;
-	
-	private final static int[] materialWeights = new int[7];
-	
-	private final static int tempoWeight = S(14, 5);
-	private final static int bishopPairWeight = S(10, 42);
-
-	private final static int[][] mobilityWeights = new int[7][];
-
-	private final static int[] zeroi = new int[2];
-	
-	private final int[] materialScore = new int[2];
 	
 	/** margin for scaling scores from midgame to endgame
 	 * <p> calculated by difference between midgame and endgame material*/
@@ -80,28 +79,6 @@ public final class E9v3 implements Evaluator3{
 		materialWeights[State4.PIECE_TYPE_BISHOP] = 306;
 		materialWeights[State4.PIECE_TYPE_KNIGHT] = 301;
 		materialWeights[State4.PIECE_TYPE_PAWN] = 100;
-
-		mobilityWeights[State4.PIECE_TYPE_KNIGHT] = new int[]{
-				S(-19,-49), S(-13,-40), S(-6,-27), S(-1,0), S(7,2),
-				S(12,10), S(14,28), S(16,44), S(17,48)
-		};
-		mobilityWeights[State4.PIECE_TYPE_BISHOP] = new int[]{
-				S(-13,-30), S(-6,-20), S(1,-18), S(7,-10), S(15,-1),
-				S(24,8), S(28,14), S(24,18), S(30,20), S(34,23),
-				S(38,25), S(43,31), S(49,32), S(55,37), S(55,38), S(55,38)
-		};
-		mobilityWeights[State4.PIECE_TYPE_ROOK] = new int[]{
-				S(-10,-69), S(-7,-47), S(-4,-43), S(-1,-10), S(2,13), S(5,26),
-				S(7,35), S(10,43), S(11,50), S(12,56), S(12,60), S(13,63),
-				S(14,66), S(15,69), S(15,74), S(17,74)
-		};
-		mobilityWeights[State4.PIECE_TYPE_QUEEN] = new int[]{
-				S(-6,-69), S(-4,-49), S(-2,-45), S(-2,-28), S(-1,-9), S(0,10),
-				S(1,15), S(2,20), S(4,25), S(5,30), S(6,30), S(7,30), S(8,30),
-				S(8,30), S(9,30), S(10,35), S(12,35), S(14,35), S(15,35), S(15,35),
-				S(15,35), S(15,35), S(15,35), S(15,35), S(15,35), S(15,35), S(15,35),
-				S(15,35), S(15,35), S(15,35), S(15,35), S(15,35)
-		};
 	}
 	
 	public E9v3(){
@@ -252,8 +229,8 @@ public final class E9v3 implements Evaluator3{
 			final long pawnAttacks = whitePawnAttacks | blackPawnAttacks;
 			final double clutterMult = clutterIndex[(int)BitUtil.getSetBits(pawnAttacks)];
 			
-			final int stage2Score = scoreMobility(player, s, clutterMult, nonPawnMaterial, attackMask) -
-					scoreMobility(1-player, s, clutterMult, nonPawnMaterial, attackMask);
+			final int stage2Score = MobilityEval.scoreMobility(player, s, clutterMult, nonPawnMaterial, attackMask) -
+					MobilityEval.scoreMobility(1-player, s, clutterMult, nonPawnMaterial, attackMask);
 			score += Weight.interpolate(stage2Score, scale) + Weight.interpolate(S((int)(Weight.egScore(stage2Score)*.1), 0), scale);
 			if(queens == 0){
 				flags |= stage3Flag;
@@ -308,8 +285,8 @@ public final class E9v3 implements Evaluator3{
 			final double clutterMult = clutterIndex[(int)BitUtil.getSetBits(pawnAttacks)];
 			
 			//recalculate attack masks
-			scoreMobility(player, s, clutterMult, nonPawnMaterial, attackMask);
-			scoreMobility(1-player, s, clutterMult, nonPawnMaterial, attackMask);
+			MobilityEval.scoreMobility(player, s, clutterMult, nonPawnMaterial, attackMask);
+			MobilityEval.scoreMobility(1-player, s, clutterMult, nonPawnMaterial, attackMask);
 
 			final int stage3Score = evalKingSafety(player, s, alliedQueens, enemyQueens);
 
@@ -537,145 +514,4 @@ public final class E9v3 implements Evaluator3{
 	
 	@Override
 	public void reset(){}
-	
-
-	/** calculates mobility and danger to enemy king from mobility*/
-	private static int scoreMobility(final int player, final State4 s,
-			final double clutterMult, final int[] nonPawnMaterial, final long[] attackMask){
-		int mobScore = 0;
-		
-		final long alliedPawns = s.pawns[player];
-		final long enemyPawns = s.pawns[1-player];
-		final long enemyPawnAttacks = Masks.getRawPawnAttacks(1-player, enemyPawns);
-		
-		final long allied = s.pieces[player];
-		final long enemy = s.pieces[1-player];
-		final long agg = allied | enemy;
-		final long aggPawns = alliedPawns | enemyPawns;
-		final long blockedAlliedPawns; //allied pawns whose movement is blocked by another pawn
-		final long chainedAlliedPawns; //allied pawns that are supporting other allied pawns
-		
-		if(player == 0){
-			final long movementBlocked = ((alliedPawns << 8) & aggPawns) >>> 8;
-			final long supportPawns = (((alliedPawns << 7) & alliedPawns) >>> 7) |
-					(((alliedPawns << 9) & alliedPawns) >>> 9);
-			blockedAlliedPawns = movementBlocked;
-			chainedAlliedPawns = supportPawns;
-		} else{
-			final long movementBlocked = ((alliedPawns >>> 8) & aggPawns) << 8;
-			final long supportPawns = (((alliedPawns >>> 7) & alliedPawns) << 7) |
-					(((alliedPawns >>> 9) & alliedPawns) << 9);
-			blockedAlliedPawns = movementBlocked;
-			chainedAlliedPawns = supportPawns;
-		}
-		
-		long bishopAttackMask = 0;
-		for(long bishops = s.bishops[player]; bishops != 0; bishops &= bishops-1){
-			final long b = bishops & -bishops;
-			final long rawMoves = Masks.getRawBishopMoves(agg, b);
-			final long moves = rawMoves & ~allied & ~enemyPawnAttacks;
-			final int count = (int)BitUtil.getSetBits(moves);
-			mobScore += Weight.multWeight(mobilityWeights[State4.PIECE_TYPE_BISHOP][count], clutterMult);
-			bishopAttackMask |= rawMoves;
-			
-			//penalize bishop for blocking allied pawns on bishop color
-			final long squareMask = (PositionMasks.bishopSquareMask[0] & b) != 0?
-					PositionMasks.bishopSquareMask[0]: PositionMasks.bishopSquareMask[1];
-			final int blockingPawnsCount = (int)BitUtil.getSetBits(blockedAlliedPawns & squareMask);
-			final int supportingPawnsCount = (int)BitUtil.getSetBits(chainedAlliedPawns & squareMask & ~blockedAlliedPawns);
-			mobScore += S(blockingPawnsCount*-5 + supportingPawnsCount*-1, 0);
-		}
-
-		long knightAttackMask = 0;
-		for(long knights = s.knights[player]; knights != 0; knights &= knights-1){
-			final long k = knights & -knights;
-			final long rawMoves = Masks.getRawKnightMoves(k);
-			final long moves = rawMoves & ~allied & ~enemyPawnAttacks;
-			final int count = (int)BitUtil.getSetBits(moves);
-			mobScore += Weight.multWeight(mobilityWeights[State4.PIECE_TYPE_KNIGHT][count], clutterMult);
-			knightAttackMask |= rawMoves;
-		}
-
-		long rookAttackMask = 0;
-		final long allPieces = s.pieces[0]|s.pieces[1];
-		final int alliedKingIndex = BitUtil.lsbIndex(s.kings[player]);
-		final int alliedKingCol = alliedKingIndex%8;
-		final int alliedKingRow = alliedKingIndex >>> 3;
-		for(long rooks = s.rooks[player]; rooks != 0; rooks &= rooks-1){
-			final long r = rooks&-rooks;
-			final long rawMoves = Masks.getRawRookMoves(agg, r);
-			final long moves = rawMoves & ~allied & ~enemyPawnAttacks;
-			final int moveCount = (int)BitUtil.getSetBits(moves);
-			mobScore += Weight.multWeight(mobilityWeights[State4.PIECE_TYPE_ROOK][moveCount], clutterMult);
-			rookAttackMask |= rawMoves;
-			
-			final int rindex = BitUtil.lsbIndex(r);
-			final int col = rindex%8;
-			if(isHalfOpen(col, enemyPawns, allPieces & ~r)){ //tests file half open
-				mobScore += S(6, 15);
-				if(((allPieces & ~r) & Masks.colMask[col]) == 0){ //tests file open
-					mobScore += S(6, 15);
-				}
-			}
-
-			final int row = rindex >>> 3;
-			if(row == alliedKingRow){
-				final int backRank = player == 0? 0: 7;
-				if(alliedKingRow == backRank && moveCount <= 4){
-					if(alliedKingCol >= 4 && col > alliedKingCol){
-						if(!s.kingMoved[player] && !s.rookMoved[player][1]){
-							mobScore += S(-20, -50); //trapped, but can castle right
-						} else{
-							mobScore += S(-50, -120); //trapped, cannot castle
-							//old, 80, 150
-						}
-					}
-					if(alliedKingCol <= 3 && col < alliedKingCol){
-						if(!s.kingMoved[player] && !s.rookMoved[player][0]){
-							mobScore += S(-20, -50); //trapped, but can castle left
-						} else{
-							mobScore += S(-50, -120); //trapped, cannot castle
-						}
-					}
-				}
-			}
-		}
-
-		long queenAttackMask = 0;
-		for(long queens = s.queens[player]; queens != 0; queens &= queens-1){
-			final long q = queens&-queens;
-			final long rawMoves = Masks.getRawQueenMoves(agg, q);
-			final long moves = rawMoves & ~allied & ~enemyPawnAttacks;
-			final int count = (int)BitUtil.getSetBits(moves);
-			mobScore += Weight.multWeight(mobilityWeights[State4.PIECE_TYPE_QUEEN][count], clutterMult);
-			queenAttackMask |= rawMoves;
-		}
-		
-		final long pawnAttackMask = Masks.getRawPawnAttacks(player, alliedPawns);
-		attackMask[player] = bishopAttackMask | knightAttackMask | rookAttackMask | queenAttackMask | pawnAttackMask;
-		
-
-		//System.out.println(player+" pawn score = ("+mgScore(mobScore)+", "+egScore(mobScore)+")");
-		return mobScore;
-	}
-	
-	/**
-	 * 
-	 * @param col
-	 * @param enemyPawns
-	 * @param pieces pieces excluding the rook to be tested
-	 * @return
-	 */
-	private static boolean isHalfOpen(final int col, final long enemyPawns, final long pieces){
-		final long mask = Masks.colMask[col];
-		if((mask & pieces) == 0) return true; //column is fully open
-		if((mask & (pieces & ~enemyPawns)) == 0){
-			//no pieces except for pawns
-			final long c = enemyPawns & mask;
-			if((c & (c-1)) == 0){
-				return true; //only one pawn in the column
-			}
-		}
-		return false;
-	}
 }
