dac61d825ebb858328de83f5d254b0f7165f85b5
Jack Crawford <jdc2172@gmail.com>
2013-10-31 20:36:54 -0400

    converted knight mobility tables to parameterized logistic function

--- src/eval/e9/mobilityEval/MobilityEval.java
+++ src/eval/e9/mobilityEval/MobilityEval.java
@@ -14,11 +14,33 @@ public final class MobilityEval {
 	private static final int[] rookMobilityWeights;
 	private static final int[] queenMobilityWeights;
 	
+	private final static class LogisticParams{
+		double lowerAsymptote, upperAsymptote;
+		LogisticParams(double lowerAsymptote, double upperAsymptote){
+			this.lowerAsymptote = lowerAsymptote;
+			this.upperAsymptote = upperAsymptote;
+		}
+	}
+	private static double logistic(double x, LogisticParams p){
+		double diff = p.upperAsymptote-p.lowerAsymptote;
+		return diff*2*(1/(1+Math.exp(-x)-.5));
+	}
+	private static void interpolate(int center, LogisticParams lower, LogisticParams upper, int[] store){
+		for(int a = 0; a < store.length; a++){
+			store[a] = Weight.encode((int)(logistic(a-center, lower)), (int)(logistic(a-center, upper)+.5));
+		}
+	}
+	
 	static{
-		knightMobilityWeights = new int[]{
+		LogisticParams knightLower = new LogisticParams(-20,20);
+		LogisticParams knightUpper = new LogisticParams(-35,35);
+		knightMobilityWeights = new int[9];
+		interpolate(3, knightLower, knightUpper, knightMobilityWeights);
+		/*knightMobilityWeights = new int[]{
 				S(-19,-49), S(-13,-40), S(-6,-27), S(-1,0), S(7,2),
 				S(12,10), S(14,28), S(16,44), S(17,48)
-		};
+		}*/;
+		
 		bishopMobilityWeights = new int[]{
 				S(-13,-30), S(-6,-20), S(1,-18), S(7,-10), S(15,-1),
 				S(24,8), S(28,14), S(24,18), S(30,20), S(34,23),
