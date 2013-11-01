0996ac764b12a3ff823b51367a23738c4398ee44
Jack Crawford <jdc2172@gmail.com>
2013-11-01 02:59:15 -0400

    code cleaning

--- src/eval/e9/mobilityEval/MobilityEval.java
+++ src/eval/e9/mobilityEval/MobilityEval.java
@@ -69,21 +69,6 @@ public final class MobilityEval {
 				S(15,35), S(15,35), S(15,35), S(15,35), S(15,35), S(15,35), S(15,35),
 				S(15,35), S(15,35), S(15,35), S(15,35), S(15,35)
 		};*/
-		
-		int[][] temp = new int[][]{
-				knightMobilityWeights,
-				bishopMobilityWeights,
-				rookMobilityWeights,
-				queenMobilityWeights
-		};
-		
-		for(int a = 0; a < temp.length; a++){
-			for(int q = 0; q < temp[a].length; q++){
-				System.out.print(Weight.mgScore(temp[a][q])+", ");
-			}
-			System.out.println();
-		}
-		
 	}
 	
 	/** build a weight scaling from passed start,end values*/
