9b7b89f8e7f83c4115068690adcb9fad2ac73e46
Jack Crawford <jdc2172@gmail.com>
2014-03-03 12:26:17 -0500

    fixed bug in position printing

--- src/chess/uci/controlExtension/PrintPositionExt.java
+++ src/chess/uci/controlExtension/PrintPositionExt.java
@@ -16,10 +16,10 @@ public class PrintPositionExt implements ControlExtension {
 
 			State4 s = pos.s;
 			String castleInfo = "";
-			if(s.kingMoved[0] && s.rookMoved[0][0]) castleInfo += "q";
-			if(s.kingMoved[0] && s.rookMoved[0][1]) castleInfo += "k";
-			if(s.kingMoved[1] && s.rookMoved[1][0]) castleInfo += "Q";
-			if(s.kingMoved[1] && s.rookMoved[1][1]) castleInfo += "K";
+			if(!s.kingMoved[0] && !s.rookMoved[0][0]) castleInfo += "q";
+			if(!s.kingMoved[0] && !s.rookMoved[0][1]) castleInfo += "k";
+			if(!s.kingMoved[1] && !s.rookMoved[1][0]) castleInfo += "Q";
+			if(!s.kingMoved[1] && !s.rookMoved[1][1]) castleInfo += "K";
 			System.out.println("castle rights: "+castleInfo);
 
 			System.out.println("\n"+pos.s);
