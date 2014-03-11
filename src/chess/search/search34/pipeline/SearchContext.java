798d110eccafd99f0df2ed099d3fead1571377ec
Jack Crawford <jdc2172@gmail.com>
2014-03-11 14:18:30 -0400

    code cleaning

--- src/chess/search/search34/pipeline/SearchContext.java
+++ src/chess/search/search34/pipeline/SearchContext.java
@@ -6,18 +6,6 @@ public final class SearchContext{
 	public final static int NODE_TYPE_CUT = 1;
 	public final static int NODE_TYPE_ALL = 0;
 
-	public final int player, alpha, beta, depth, stackIndex;
-	public final int nt;
-
-	public SearchContext(int player, int alpha, int beta, int depth, int nt, int stackIndex){
-		this.player = player;
-		this.alpha = alpha;
-		this.beta = beta;
-		this.depth = depth;
-		this.nt = nt;
-		this.stackIndex = stackIndex;
-	}
-
 	public static int nextNodeType(final int nodeType){
 		return (nodeType+1) % 2;
 	}
